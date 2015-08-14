7f2b36ee6f8cb2512fede22b76540fe8de04965c
bobosana <bobosana@users.noreply.github.com>
2015-08-14 16:47:38 +0530

    sub method added
    Added the sub method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,4 +12,11 @@ public class HelloWorld {
 	public int add(int a,int b) {
 		return a+b;
 	}
+	
+		/*Adding the sub method
+		This method subtracts up two integers passed as parameters and returns the difference.
+	*/
+	public int sub(int a,int b) {
+		return a-b;
+	}
 }
