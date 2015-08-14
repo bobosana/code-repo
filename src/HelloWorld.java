37b410e8f5a4a19bcf8e95004d6cad4d295aec5a
bobosana <bosna.nao@gmail.com>
2015-08-14 15:53:24 +0530

    Added the add method
    Added implementation of the add method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,11 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hellow world");
 	}
-
+	
+	/*Adding the add method
+		This method adds up two integers passed as parameters and returns the sum.
+	*/
+	public int add(int a,int b) {
+		return a+b;
+	}
 }
