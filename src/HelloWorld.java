77310545c3b9b0d4cc0090c2a91e478aa8723b8a
bobosana <bosna.nao@gmail.com>
2015-09-05 02:05:16 +0530

    Added class file Methods.java and moved all methods from HelloWorld.java to the new class file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,19 +4,5 @@ public class HelloWorld {
 	
 	public static void main(String args[]) {
 		System.out.println("Hellow world");
-	}
-	
-	/*Adding the add method
-		This method adds up two integers passed as parameters and returns the sum.
-	*/
-	public int add(int a,int b) {
-		return a+b;
-	}
-	
-		/*Adding the sub method
-		This method subtracts up two integers passed as parameters and returns the difference.
-	*/
-	public int sub(int a,int b) {
-		return a-b;
-	}
+	} 
 }
---
+++ src/Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+	
+		/*Adding the sub method
+		This method subtracts up two integers passed as parameters and returns the difference.
+	*/
+	public int sub(int a,int b) {
+		return a-b;
+	}
+}
